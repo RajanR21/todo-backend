228565720d983afdbef3acbfdce621de087d7fa5
Rajan Ravisaheb <rajanroy220@gmail.com>
2023-08-12 16:37:53 +0530

    middleware is to create the auth token to check user login activity

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+import { user } from "../models/user.js"
+import jwt from "jsonwebtoken"
+export const isAuthenticated = async(req , res , next)=> {
+   
+    const {token} = req.cookies
+
+    if(!token){
+        return res.status(404).json({
+            success: false,
+            message: "log in first",
+        })
+    }
+    
+    const decoded = jwt.verify(token , "nwiieowwefecoc")
+    req.user = await user.findById(decoded._id);
+    next();
+}
